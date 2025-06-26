fd8e6bd850ccb7aa97e1533ee13a1d872200753c
eliasfernandescout <eliasfernandescout@gmail.com>
2025-06-26 17:19:47 -0300

    Add H2 server configuration and update DataSourceConfig for TCP connections

---
+++ src/main/java/com/automak_sensors/automak_temperature_processing/api/config/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.automak_sensors.automak_temperature_processing.api.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    @DependsOn("h2Server")
+    public DataSource dataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
+    }
+}
+
